f2ab5d7be1ec9ca44ef241174132691f2c83bcaa
martyn.eggleton <martyn.eggleton@7a90bdde-fc53-11de-8802-f5e385a79211>
2010-01-14 20:29:06 +0000

    makes the dokuwiki plugin work try with the following code
    <stretchqi notation=sensible renderer=text>
    Ch3-e3, Hh10-g8
    </stretchqi>
    
    
    <stretchqi>
    Ch3-e3, Hh10-g8
    </stretchqi>
    
    
    
    <stretchqi renderer=table>
    Ch3-e3, Hh10-g8
    </stretchqi>

--- syntax.php
+++ syntax.php
@@ -41,47 +41,48 @@ class syntax_plugin_stretchqi extends DokuWiki_Syntax_Plugin {
     }
 
     function handle($match, $state, $pos, &$handler){
-	
-	
-        $data = explode("\n",$match);
-        $conf  = array_shift($data);
-        array_pop($data);
-        $sConf  = trim(substr($conf,10,-1));
-	
-	$aConfStrings = split(' ', trim($sConf));
-	
-	$aConf = array(); 
-	
-	foreach($aConfStrings as $sConfString)
-	{
-		$aParts = split('=', $sConfString);
-		$aConf[$aParts[0]] = $aParts[1];
-	}
-		
-	//var_dump($data);
-
-	//var_dump($aConf);
-
-        // parse
-        return array(
-            'conf'  => $aConf,
-            'data'  => join("\n", $data),
-            );
+      
+        $aData = array('notation'=>'sensible','renderer'=>'text');
+        $aLines = explode("\n",$match);
+        $sConf  = array_shift($aLines);
+        array_pop($aLines);
+        $aData['text'] = join("\n", $aLines);
+        $sConf  = trim(substr($sConf,10,-1));
+        
+        $aConfStrings = split(' ', trim($sConf));
+        $aConf = array(); 
+        foreach($aConfStrings as $sConfString)
+        {
+          $aParts = split('=', $sConfString);
+          $aConf[$aParts[0]] = $aParts[1];
+        }
+        
+        if(isset($aConf['renderer']))
+        {
+          $aData['renderer'] = $aConf['renderer'];
+        }
+        
+        if(isset($aConf['notation']))
+        {
+          $aData['notation'] = $aConf['notation'];
+        }
+        
+        return $aData;
     }
 
     function render($mode, &$renderer, $data) {
         if($mode != 'xhtml') return false;
-		
-	require_once('stretchqilib/xiangqi.php');
+        
+        require_once('stretchqilib/xiangqi.php');
   
-  	$oBoard = new xiangqi();
-  	$oBoard->parseNotation($data, $conf['notation']);
-  
-  	$oRenderer = $oBoard->getRenderer($conf['renderer']);
-  	$sBoard = $oRenderer->getHTML();
-	
-	$renderer->doc .= $sBoard;
-	
+        $oBoard = new xiangqi();
+        $oBoard->parseNotation($data['text'], $data['notation']);
+        
+        $oRenderer = $oBoard->getRenderer($data['renderer']);
+        $oRenderer->sPiecePath = 'lib/plugins/stretchqi/pieces/1/';
+        $sBoard = $oRenderer->getHTML();
+        
+        $renderer->doc .= $sBoard;
         return true;
     }
 }
